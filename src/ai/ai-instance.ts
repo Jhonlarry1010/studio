c3801ce7d32b72f674ec04cceafb172ad3f3ed89
Jhonlarry1010 <nanthanantha1223@gmail.com>
2025-05-15 01:00:36 -0700

    Update ai-instance.ts

--- src/ai/ai-instance.ts
+++ src/ai/ai-instance.ts
@@ -6,7 +6,17 @@ export const ai = genkit({
   plugins: [
     googleAI({
       apiKey: process.env.GOOGLE_GENAI_API_KEY,
+      GOOGLE_GENAI_API_KEY=AIzaSyBatfrTOriZzq0s_NUwJm_-fYlUra5mxQk
     }),
   ],
   model: 'googleai/gemini-2.0-flash',
+  export const ai = genkit({
+  promptDir: './prompts',
+  plugins: [
+    googleAI({
+      apiKey: 'AIzaSyBatfrTOriZzq0s_NUwJm_-fYlUra5mxQk',
+    }),
+  ],
+  model: 'googleai/gemini-2.0-flash',
+});
 });
